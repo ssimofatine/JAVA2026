566d14f331e658adb6edcfc3e6bfa556a1d1b7c8
ssimofatine <simohaji147@gmail.com>
2026-03-01 21:38:35 +0100

    edit

---
+++ tema6/Practica_Objetos_III/test1.java
@@ -0,0 +1,7 @@
+package tema6.Practica_Objetos_III;
+
+public class test1 {
+    static void main(String[] args) {
+
+    }
+}
---
+++ tema6/Practica_en_grupo_Juego_rpg/test.java
@@ -0,0 +1,7 @@
+package tema6.Practica_en_grupo_Juego_rpg;
+
+public class test {
+    static void main(String[] args) {
+
+    }
+}
