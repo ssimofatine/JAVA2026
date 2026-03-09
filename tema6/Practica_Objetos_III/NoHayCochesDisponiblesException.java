6d76f496ecb394b9219fe7614d1b22ed454976fe
ssimofatine <simohaji147@gmail.com>
2026-03-09 20:04:08 +0100

    edit

---
+++ tema6/Practica_Objetos_III/NoHayCochesDisponiblesException.java
@@ -0,0 +1,7 @@
+package tema6.Practica_Objetos_III;
+
+public class NoHayCochesDisponiblesException extends RuntimeException {
+    public NoHayCochesDisponiblesException(String message) {
+        super(message);
+    }
+}
